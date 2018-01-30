c63c4257b1d779f1f66a178c6cb8f903dce30885
ThuanTHM <makuryuuu@gmail.com>
2018-01-30 09:15:59 +0700

    Update view select-feedback-target & progress bar

--- Source Code/startbootstrap-sb-admin-gh-pages/js/select-feedback-target.js
+++ Source Code/startbootstrap-sb-admin-gh-pages/js/select-feedback-target.js
@@ -1,9 +1,32 @@
 /**
  * Created by MyPC on 29/01/2018.
  */
-$(document).ready(function() {
+var selectedtarget;
+var tmp;
+$(document).ready(function () {
     $('#tbl-courses').DataTable();
     $('#tbl-lecturers').DataTable();
     $('#tbl-departments').DataTable();
     $('#tbl-subjects').DataTable();
 });
+$(".btn-check-target").click(function () {
+    tmp = $(this).val();
+    alert(tmp);
+    if (selectedtarget != null) {
+        selectedtarget.removeClass("btn-checked");
+        selectedtarget.addClass("btn-check-target");
+        selectedtarget.val("Chọn");
+    }
+    if (tmp == "Chọn") {
+        selectedtarget = $(this);
+        selectedtarget.removeClass("btn-check-target");
+        selectedtarget.addClass("btn-checked");
+        selectedtarget.val("Đã chọn");
+    }
+});
+// $(".btn-checked").click(function(){
+//     selectedtarget.removeClass("btn-checked");
+//     selectedtarget.addClass("btn-check-target");
+//     selectedtarget.text("Chọn");
+//     selectedtarget = null;
+// });
