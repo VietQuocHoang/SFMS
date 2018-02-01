14c1d022f0a448d5f4d5a08cc5031d1914482752
ThuanTHM <makuryuuu@gmail.com>
2018-02-01 12:44:33 +0700

    remove old maven project

--- Source Code/SFMS/src/main/java/com/sample/sfms/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sample.sfms.repository;
-
-import com.sample.sfms.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-    User findByEmail(String email);
-}
--- Source Code/SFMS/src/main/resources/static/js/select-feedback-target.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by MyPC on 29/01/2018.
- */
-$(document).ready(function() {
-    $('#tbl-courses').DataTable();
-    $('#tbl-lecturers').DataTable();
-    $('#tbl-departments').DataTable();
-    $('#tbl-subjects').DataTable();
-});
