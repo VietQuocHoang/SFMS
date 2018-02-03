7930000b158d1258031b85c16bab7bf0aed2c28b
ThuanTHM <makuryuuu@gmail.com>
2018-02-04 00:59:58 +0700

    Fix all select feedback object views

--- Source Code/sfms/src/main/resources/static/js/select-feedback-target.js
+++ Source Code/sfms/src/main/resources/static/js/select-feedback-target.js
@@ -4,10 +4,118 @@
 var selectedtarget;
 var tmp;
 $(document).ready(function () {
-    $('#tbl-courses').DataTable();
-    $('#tbl-lecturers').DataTable();
-    $('#tbl-departments').DataTable();
-    $('#tbl-subjects').DataTable();
+    $('#tbl-courses').DataTable(
+        {
+            "language": {
+                "decimal": "",
+                "emptyTable": "Không kết quả nào được tìm thấy",
+                "info": "Hiển thị từ _START_ tới _END_ trong số _TOTAL_ kết quả",
+                "infoEmpty": "Hiển thị 0 tới 0 trong số 0 kết quả",
+                "infoFiltered": "(filtered from _MAX_ total entries)",
+                "infoPostFix": "",
+                "thousands": ",",
+                "lengthMenu": "_MENU_ kết quả mỗi trang",
+                "loadingRecords": "Đang tải...",
+                "processing": "Đang xử lý...",
+                "search": "Tìm:",
+                "zeroRecords": "Không tìm thấy kết quả phù hợp",
+                "paginate": {
+                    "first": "Đầu",
+                    "last": "Trước",
+                    "next": "Sau",
+                    "previous": "Cuối"
+                },
+                "aria": {
+                    "sortAscending": ": kích hoạt để sắp xếp tăng dần",
+                    "sortDescending": ": kích hoạt để sắp xếp giảm dần"
+                }
+            }
+        }
+    );
+    $('#tbl-lecturers').DataTable(
+        {
+            "language": {
+                "decimal": "",
+                "emptyTable": "Không kết quả nào được tìm thấy",
+                "info": "Hiển thị từ _START_ tới _END_ trong số _TOTAL_ kết quả",
+                "infoEmpty": "Hiển thị 0 tới 0 trong số 0 kết quả",
+                "infoFiltered": "(filtered from _MAX_ total entries)",
+                "infoPostFix": "",
+                "thousands": ",",
+                "lengthMenu": "_MENU_ kết quả mỗi trang",
+                "loadingRecords": "Đang tải...",
+                "processing": "Đang xử lý...",
+                "search": "Tìm:",
+                "zeroRecords": "Không tìm thấy kết quả phù hợp",
+                "paginate": {
+                    "first": "Đầu",
+                    "last": "Trước",
+                    "next": "Sau",
+                    "previous": "Cuối"
+                },
+                "aria": {
+                    "sortAscending": ": kích hoạt để sắp xếp tăng dần",
+                    "sortDescending": ": kích hoạt để sắp xếp giảm dần"
+                }
+            }
+        }
+    );
+    $('#tbl-departments').DataTable(
+        {
+            "language": {
+                "decimal": "",
+                "emptyTable": "Không kết quả nào được tìm thấy",
+                "info": "Hiển thị từ _START_ tới _END_ trong số _TOTAL_ kết quả",
+                "infoEmpty": "Hiển thị 0 tới 0 trong số 0 kết quả",
+                "infoFiltered": "(filtered from _MAX_ total entries)",
+                "infoPostFix": "",
+                "thousands": ",",
+                "lengthMenu": "_MENU_ kết quả mỗi trang",
+                "loadingRecords": "Đang tải...",
+                "processing": "Đang xử lý...",
+                "search": "Tìm:",
+                "zeroRecords": "Không tìm thấy kết quả phù hợp",
+                "paginate": {
+                    "first": "Đầu",
+                    "last": "Trước",
+                    "next": "Sau",
+                    "previous": "Cuối"
+                },
+                "aria": {
+                    "sortAscending": ": kích hoạt để sắp xếp tăng dần",
+                    "sortDescending": ": kích hoạt để sắp xếp giảm dần"
+                }
+            }
+        }
+    );
+    $('#tbl-subjects').DataTable(
+        {
+            "language": {
+                "decimal": "",
+                "emptyTable": "Không kết quả nào được tìm thấy",
+                "info": "Hiển thị từ _START_ tới _END_ trong số _TOTAL_ kết quả",
+                "infoEmpty": "Hiển thị 0 tới 0 trong số 0 kết quả",
+                "infoFiltered": "(filtered from _MAX_ total entries)",
+                "infoPostFix": "",
+                "thousands": ",",
+                "lengthMenu": "_MENU_ kết quả mỗi trang",
+                "loadingRecords": "Đang tải...",
+                "processing": "Đang xử lý...",
+                "search": "Tìm:",
+                "zeroRecords": "Không tìm thấy kết quả phù hợp",
+                "paginate": {
+                    "first": "Đầu",
+                    "last": "Trước",
+                    "next": "Sau",
+                    "previous": "Cuối"
+                },
+                "aria": {
+                    "sortAscending": ": kích hoạt để sắp xếp tăng dần",
+                    "sortDescending": ": kích hoạt để sắp xếp giảm dần"
+                }
+            }
+        }
+    );
 });
 $(".btn-check-target").click(function () {
     tmp = $(this).val();
