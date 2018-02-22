c8b9ad63b0d40d04314cd56ff0593f97f5708ed5
Tu Nguyen <tu@instaroid.sg>
2018-02-22 23:52:44 +0700

    Add load template content service

--- Source Code/SFMS/src/main/java/com/sample/sfms/api/FeedbackAPI.java
+++ Source Code/SFMS/src/main/java/com/sample/sfms/api/FeedbackAPI.java
@@ -4,6 +4,7 @@ import com.sample.sfms.entity.Feedback;
 import com.sample.sfms.service.interf.FeedbackService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +25,10 @@ public class FeedbackAPI {
     @GetMapping
     private List<Feedback> getListTemplate(){
         return feedbackService.getListTemplate(true);
+    }
+
+    @GetMapping("/{id}")
+    private Feedback getTemplateContent(@PathVariable("id") int id){
+        return feedbackService.findFeedbackById(id);
     }
 }
--- Source Code/SFMS/src/main/java/com/sample/sfms/repository/FeedbackRepository.java
+++ Source Code/SFMS/src/main/java/com/sample/sfms/repository/FeedbackRepository.java
@@ -8,4 +8,6 @@ import java.util.List;
 @org.springframework.stereotype.Repository
 public interface FeedbackRepository extends JpaRepository<Feedback, Integer> {
    List<Feedback> findByIsTemplate(boolean isTemplate);
+
+   Feedback findById(int id);
 }
