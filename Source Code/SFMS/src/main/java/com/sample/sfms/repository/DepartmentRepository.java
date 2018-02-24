397e80e7220f26e4bc4ceeab297c18f811ca69d0
Tu Nguyen <tu@instaroid.sg>
2018-02-25 02:54:17 +0700

    Add Load + filter list report

--- Source Code/SFMS/src/main/java/com/sample/sfms/repository/DepartmentRepository.java
+++ Source Code/SFMS/src/main/java/com/sample/sfms/repository/DepartmentRepository.java
@@ -2,7 +2,10 @@ package com.sample.sfms.repository;
 
 import com.sample.sfms.entity.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * Created by MyPC on 23/02/2018.
@@ -11,4 +14,8 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DepartmentRepository extends JpaRepository<Department, Integer> {
+
+    @Query("select d from Department d where " +
+            "EXISTS (SELECT f FROM Feedback f WHERE f.departmentByDepartmentId.id = d.id)")
+    List<Department> filtering ();
 }
