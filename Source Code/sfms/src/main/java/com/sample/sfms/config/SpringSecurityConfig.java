9a14743b9b3c7c4726fa46ad30738fed3c2697a4
ThuanTHM <makuryuuu@gmail.com>
2018-03-06 23:55:10 +0700

    ahihi

--- Source Code/sfms/src/main/java/com/sample/sfms/config/SpringSecurityConfig.java
+++ Source Code/sfms/src/main/java/com/sample/sfms/config/SpringSecurityConfig.java
@@ -33,39 +33,39 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.debug(true)
-                .ignoring().antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/img/**", "/vendor/**");
+//        web.debug(true)
+                web.ignoring().antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/img/**", "/vendor/**");
     }
 
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.jdbcAuthentication().usersByUsernameQuery(usersQuery)
-                .authoritiesByUsernameQuery(privilegesQuery)
-                .dataSource(dataSource)
-                .passwordEncoder(bCryptPasswordEncoder);
+//        auth.jdbcAuthentication().usersByUsernameQuery(usersQuery)
+//                .authoritiesByUsernameQuery(privilegesQuery)
+//                .dataSource(dataSource)
+//                .passwordEncoder(bCryptPasswordEncoder);
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/", "/login").permitAll()
-                .antMatchers("/roles/**").hasAuthority("EDIT_PERMISSION")
-                .antMatchers("/users/**").access("hasAuthority('EDIT_PERMISSION') or hasAuthority('EDIT_FEEDBACK')")
-                .antMatchers("/feedbacks/**").access("hasAuthority('EDIT_PERMISSION') or hasAuthority('EDIT_FEEDBACK')")
-                .mvcMatchers("/conduct-feedback/**").access("hasAuthority('CONDUCT_FEEDBACK')")
-                .anyRequest().authenticated()
-                .and()
-                .formLogin().loginPage("/login").failureUrl("/login?error=true")
-                .usernameParameter("username")
-                .passwordParameter("password")
-                .defaultSuccessUrl("/home")
-                .and()
-                .rememberMe().rememberMeParameter("remember-me")
-                .and()
-                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .logoutSuccessUrl("/").and().exceptionHandling()
-                .accessDeniedPage("/access-denied");
+//        http.csrf().disable()
+//                .authorizeRequests()
+//                .antMatchers("/", "/login").permitAll()
+//                .antMatchers("/roles/**").hasAuthority("EDIT_PERMISSION")
+//                .antMatchers("/users/**").access("hasAuthority('EDIT_PERMISSION') or hasAuthority('EDIT_FEEDBACK')")
+//                .antMatchers("/feedbacks/**").access("hasAuthority('EDIT_PERMISSION') or hasAuthority('EDIT_FEEDBACK')")
+//                .mvcMatchers("/conduct-feedback/**").access("hasAuthority('CONDUCT_FEEDBACK')")
+//                .anyRequest().authenticated()
+//                .and()
+//                .formLogin().loginPage("/login").failureUrl("/login?error=true")
+//                .usernameParameter("username")
+//                .passwordParameter("password")
+//                .defaultSuccessUrl("/home")
+//                .and()
+//                .rememberMe().rememberMeParameter("remember-me")
+//                .and()
+//                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+//                .logoutSuccessUrl("/").and().exceptionHandling()
+//                .accessDeniedPage("/access-denied");
     }
 }
