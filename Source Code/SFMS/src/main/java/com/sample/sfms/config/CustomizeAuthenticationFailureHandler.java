79609634fff5d21e6521eed369961b3339bff35f
MarcusFenix1 <viethoangquoc2711@gmail.com>
2018-04-11 15:00:04 +0700

    - Customize Success And Failure Login Handler

---
+++ Source Code/SFMS/src/main/java/com/sample/sfms/config/CustomizeAuthenticationFailureHandler.java
@@ -0,0 +1,19 @@
+package com.sample.sfms.config;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CustomizeAuthenticationFailureHandler implements AuthenticationFailureHandler {
+    private final int FORBIDDEN = 403;
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        response.setContentType("application/json");
+        response.setStatus(FORBIDDEN);
+    }
+}
